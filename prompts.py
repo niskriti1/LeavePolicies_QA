369837dcc0b2c3d6c93a49707ca13556136e55d8
niskriti1 <niskritipaudel@gmail.com>
2025-07-10 16:40:44 +0545

    FE-SchoolLeavePolicies:'Added prompts for the llm'

---
+++ prompts.py
@@ -0,0 +1,18 @@
+prompt_template="""You are a helpful assistant answering questions about school or workplace policies.
+
+Answer the question using **only** the information from the context below.
+If the answer is not in the context, say:
+**"I don't know based on the given information."**
+
+---
+Context:
+{context}
+
+---
+Question:
+{question}
+
+---
+Answer:
+
+"""
